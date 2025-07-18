c788a1b730298f006a75bd81b32ed5df2ab786d4
tsion bantegize <166711429+tsionbantegize16@users.noreply.github.com>
2025-07-18 14:36:09 +0300

    first commit

---
+++ src/main/java/com/example/miniuniconnect/MiniuniconnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniuniconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniuniconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniuniconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniuniconnect/MiniuniconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniuniconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniuniconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
